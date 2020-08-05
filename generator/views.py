07b057cdac44bf178f77a4f43debdd362fa125eb
pooja srinivasan <58687165+poojaseenu1999@users.noreply.github.com>
2020-08-06 01:33:47 +0530

    adding cases

--- generator/views.py
+++ generator/views.py
@@ -8,7 +8,16 @@ def home(request):
 def password(request):
 
    characters = list('abcdefghijklmnopqrstuvwxyz')
-   length = 10
+   if request.GET.get('uppercase'):
+     characters.extend(list('ABCDEFGHIJKLMNOPQRSTUVWXYZ'))
+
+   if request.GET.get('special'):
+       characters.extend(list('!@#$%^&*)('))
+
+   if request.GET.get('numbers'):
+       characters.extend(list('1234567890'))
+
+   length = int(request.GET.get('length',12))
    thepassword = ''
 
    for x in range(length):
