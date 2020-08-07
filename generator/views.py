b6d5e73d0bbb2712c30a412aa30205c368cbec15
pooja srinivasan <58687165+poojaseenu1999@users.noreply.github.com>
2020-08-07 14:02:54 +0530

    adding about page

--- generator/views.py
+++ generator/views.py
@@ -5,6 +5,11 @@ import random
 # Create your views here.
 def home(request):
     return render(request,'generator/home.html')
+
+def about(request):
+    return render(request,'generator/about.html')
+
+
 def password(request):
 
    characters = list('abcdefghijklmnopqrstuvwxyz')
